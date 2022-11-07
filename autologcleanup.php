3c770e2886f0e3976483ee71f42c98566738afdd
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2022-11-07 12:08:37 +0100

    MAE-793: Initialize extension with boilerplate

---
+++ autologcleanup.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once 'autologcleanup.civix.php';
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function autologcleanup_civicrm_config(&$config) {
+  _autologcleanup_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function autologcleanup_civicrm_install() {
+  _autologcleanup_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function autologcleanup_civicrm_postInstall() {
+  _autologcleanup_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function autologcleanup_civicrm_uninstall() {
+  _autologcleanup_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function autologcleanup_civicrm_enable() {
+  _autologcleanup_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function autologcleanup_civicrm_disable() {
+  _autologcleanup_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function autologcleanup_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _autologcleanup_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function autologcleanup_civicrm_entityTypes(&$entityTypes) {
+  _autologcleanup_civix_civicrm_entityTypes($entityTypes);
+}
